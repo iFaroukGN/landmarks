08240193e2c4adff7867234efb4202344512a12b
Farouk <farouk@appsolute.fr>
2018-08-16 09:26:27 +0200

    Initial Commit

---
+++ landmark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  landmark
+//
+//  Created by Farouk GN on 16/08/2018.
+//  Copyright © 2018 Farouk GN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
